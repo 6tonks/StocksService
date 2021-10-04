8474ad68c0c55102b5f3fa6c136f47fc5953460d
Robert Kim <robert23kim@gmail.com>
2021-10-04 10:03:37 -0400

    parsing and batch query for 500 stocks

--- FlaskProject/application_services/StocksResource/stocks_resource.py
+++ FlaskProject/application_services/StocksResource/stocks_resource.py
@@ -10,11 +10,21 @@ class StocksResource(BaseApplicationResource):
     def __init__(self):
         super().__init__()
 
+    def chunks(lst, n):
+        """Yield successive n-sized chunks from lst"""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
     @classmethod
     def get_stocks(cls):
         stocks = pd.read_csv('sp_500_stocks.csv')
-        symbol = 'AAPL'
-        api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote/?token={IEX_CLOUD_API_TOKEN}'
-        data = requests.get(api_url).json()
-        print(data)
+        symbol_groups = list(cls.chunks(stocks['Ticker'], 100))
+        symbol_strings = []
+        for i in range(0, len(symbol_groups)):
+            symbol_strings.append(','.join(symbol_groups[i]))
+        for symbol_string in symbol_strings:
+            batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=company,quote&token={IEX_CLOUD_API_TOKEN}'
+            data = requests.get(batch_api_call_url).json()
+            for symbol in symbol_string.split(','):
+                print(data[symbol]['company']['companyName'],symbol,data[symbol]['quote']['latestPrice'])
         return 1
